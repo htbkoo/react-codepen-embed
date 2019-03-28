6e683da58b4a720bd43a950f07b67c17c17e5e7a
htbkoo <h1017760@connect.hku.hk>
2019-03-28 08:33:40 +0800

    refactored CodepenEmbedScriptTagBuilder.test.ts htbkoo/react-codepen-embed

--- test/CodepenEmbedScriptTagBuilder.test.ts
+++ test/CodepenEmbedScriptTagBuilder.test.ts
@@ -10,10 +10,12 @@ describe('CodepenEmbedScriptTagBuilder', function () {
         builder.appendTo(mockElement, () => mockScriptTag as any);
 
         // then
-        expect(mockScriptTag.src).toEqual('https://production-assets.codepen.io/assets/embed/ei.js');
-        expect(mockScriptTag.async).toEqual(false);
-        expect(mockScriptTag.onload).toEqual(null);
-        expect(mockScriptTag.onerror).toEqual(null);
+        expect(mockScriptTag).toMatchObject({
+            src: 'https://production-assets.codepen.io/assets/embed/ei.js',
+            async: false,
+            onload: null,
+            onerror: null,
+        });
 
         expect(mockElement.appendChild).toBeCalledWith(mockScriptTag)
     });
