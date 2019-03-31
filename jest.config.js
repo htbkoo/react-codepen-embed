8de0807888042c29435f99b6440923ece08d5575
htbkoo <h1017760@connect.hku.hk>
2019-03-31 18:07:01 +0800

    refactored `jest.config.js`

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,22 @@
-module.exports = {
+const commonConfig = {
     preset: 'ts-jest',
+    setupFiles: ["./test/enzymeSetup.ts"],
+};
+
+module.exports = {
     projects: [
         {
-            preset: 'ts-jest',
-            setupFiles: ["./test/enzymeSetup.ts"],
+            ...commonConfig,
             displayName: "Unit tests",
             testRegex: ["(/test/unit/.*(test|spec))\\.[jt]sx?$"],
             // testMatch: ["**/test/unit/**/?(*.)+(spec|test).[jt]s?(x)" ],
             testEnvironment: "node"
         },
         {
-            preset: 'ts-jest',
-            setupFiles: ["./test/enzymeSetup.ts"],
-            // testMatch: ["**/test/integration/**/?(*.)+(spec|test).[jt]s?(x)" ],
-            testRegex: ["(/test/integration/.*(test|spec))\\.[jt]sx?$"],
+            ...commonConfig,
             displayName: "Integration tests",
+            testRegex: ["(/test/integration/.*(test|spec))\\.[jt]sx?$"],
+            // testMatch: ["**/test/integration/**/?(*.)+(spec|test).[jt]s?(x)" ],
             testEnvironment: 'jsdom',
         },
     ]
