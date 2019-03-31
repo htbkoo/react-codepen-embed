402d85b58cdcb3f5ee69c46e9d1b92c7f75ebf2b
htbkoo <h1017760@connect.hku.hk>
2019-03-31 11:39:07 +0800

    created `ReactCodepen.test.tsx`

---
+++ test/ReactCodepen.test.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {when} from 'jest-when'
+import {shallow} from "enzyme";
+
+import ReactCodepen from "../src";
+
+describe('<ReactCodepen/>', function () {
+    it('should mount without error', function () {
+        // given
+        shallow(<ReactCodepen hash="someHash" user="someUser" shouldLoadScript={false}/>);
+        // when
+        // then
+    });
+});
