46a52e65514c1edf68afd0762921ef25eb17d07b
htbkoo <h1017760@connect.hku.hk>
2019-04-01 08:26:09 +0800

    refactored

--- test/unit/ReactCodepen.test.tsx
+++ test/unit/ReactCodepen.test.tsx
@@ -8,7 +8,7 @@ describe('<ReactCodepen/>', function () {
     it('should render "null" if not loaded and no loader provided', function () {
         // given
         // when
-        const wrapper = shallow(<ReactCodepen hash="someHash" user="someUser" shouldLoadScript={false}/>);
+        const wrapper = newWrapper();
 
         // then
         expect(wrapper.isEmptyRender()).toBe(true);
@@ -19,11 +19,20 @@ describe('<ReactCodepen/>', function () {
         const loader = <div id="loader"/>;
 
         // when
-        const wrapper = shallow(<ReactCodepen hash="someHash" user="someUser" shouldLoadScript={false}
-                                              loader={() => loader}/>);
+        const wrapper = newWrapper({loader: () => loader});
 
         // then
         expect(wrapper.exists(".codepen")).toEqual(true);
         expect(wrapper.find(".codepen").exists("div")).toEqual(true);
     });
+
+    function newWrapper({loader = undefined} = {}) {
+        return shallow(
+            <ReactCodepen
+                hash="someHash"
+                user="someUser"
+                shouldLoadScript={false}
+                loader={loader}/>
+        );
+    }
 });
