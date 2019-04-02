4edc30578e198b58b42cb2b06f7fee3060b00a16
htbkoo <h1017760@connect.hku.hk>
2019-04-02 17:23:13 +0800

    refactored `ReactCodepen.integration.spec.tsx`

--- test/integration/ReactCodepen.integration.spec.tsx
+++ test/integration/ReactCodepen.integration.spec.tsx
@@ -3,26 +3,34 @@ import {mount} from "enzyme";
 import renderer from 'react-test-renderer';
 
 import ReactCodepen from "../../src";
+import {ReactCodepenProps} from "../../src/ReactCodepen";
 
 describe('<ReactCodepen/> - integration tests', function () {
-    it('should mount without error and render with ".codepen"', function () {
-        // given
-        // when
-        const wrapper = mount(reactCodepen());
+    describe('Not loading script', function () {
+        it('should mount without error and render with ".codepen"', function () {
+            // given
+            // when
+            const wrapper = mount(reactCodepen());
 
-        // then
-        expect(wrapper.find(".codepen").length).toEqual(1);
+            // then
+            expect(wrapper.find(".codepen").length).toEqual(1);
+        });
+
+        it('should render and match the snapshot', function () {
+            const tree = renderer
+                .create(reactCodepen())
+                .toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
     });
 
-    it('should render and match the snapshot', function () {
-        const tree = renderer
-            .create(reactCodepen())
-            .toJSON();
-
-        expect(tree).toMatchSnapshot();
-    });
-
-    function reactCodepen() {
-        return <ReactCodepen hash="someHash" user="someUser" shouldLoadScript={false}/>;
+    function reactCodepen(overrides: Partial<ReactCodepenProps> = {}) {
+        return <ReactCodepen
+            hash="someHash"
+            user="someUser"
+            shouldLoadScript={false}
+            {...overrides}
+        />;
     }
 });
